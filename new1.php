300a33ae6b3ff81ab56c31fd9c03d360c758b701
Marcin Czeszowic <marcinczeszowic@gmail.com>
2022-02-27 19:33:12 +0100

    cw_27022022

---
+++ new1.php
@@ -0,0 +1,4 @@
+<h1> witajcie </h1>
+<?php
+echo 'siema';
+?>
--- tekst1.php
+++ tekst1.php
@@ -1,11 +1,12 @@
 <h1> witajcie na zajeciach PAI </h1>
 
 <?php
-echo '<b>witaj w kodzie PHP</b><br><br>';
+//echo '<b>witaj w kodzie PHP</b><br><br>';
 
 for ($i=1 ; $i<=100 ; $i++)
     { 
     echo $i;
     }
+
 
 ?>
